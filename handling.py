935508046a7fb2340e0da6bfc947096ecb34cfae
rashikanojia <rashikanojia27@gmail.com>
2018-07-04 10:52:07 +0530

    Error handling basic code

---
+++ handling.py
@@ -0,0 +1,24 @@
+
+
+
+try:
+    p = 4565
+    b = 55689
+
+    #p = b/0
+
+
+
+    f = open("ac.txt")
+    for line in f:
+        print(line)
+
+except FileNotFoundError:
+    print("You shouldn't have done this to me")
+
+#except ZeroDivisionError:
+    #print("Divided by zero Punkass kid")
+
+
+#i = 0/0
+
